eb532a0d2d85ea0d356b93a850af85b7d18082b4
Muhammad Saqlain <muhammadsaqlain.dev@gmail.com>
2023-03-08 21:20:35 +0500

    Bug Solutions

--- lib/features/get_started_screen/first_screen.dart
+++ lib/features/get_started_screen/first_screen.dart
@@ -5,13 +5,14 @@ class FirstScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
+    var mediaQuery=MediaQuery.of(context);
     return Scaffold(
       backgroundColor: const Color(0xFFFDB515).withOpacity(0.82),
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(20.0),
-          width: 400,
-          height: 550,
+          width: mediaQuery.size.width*0.9,
+          height: mediaQuery.size.height*0.95,
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(32), color: Colors.white),
           child: Row(
@@ -22,18 +23,18 @@ class FirstScreen extends StatelessWidget {
               Image.asset(
                 'assets/logo.png',
                 alignment: Alignment.centerRight,
-                width: 100,
-                height: 100,
+                width: 75,
+                height: 75,
               ),
 
               // ignore: prefer_const_constructors
-              SizedBox(
-                width: 20,
-              ),
-              const Text(
-                'KHUDA KA KALAM',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
+              // SizedBox(
+              //   width: 20,
+              // ),
+              // const Text(
+              //   'KHUDA KA KALAM',
+              //   style: TextStyle(fontWeight: FontWeight.bold),
+              // ),
             ],
           ),
         ),
