9fa85474e5f1777b7914c71a4793143f1a4e1243
Noor Illahi <ujjannoorillahi@gmail.com>
2023-03-11 10:50:26 +0500

    Previos white box allignment fixed, urdu title and subtitle added and someother allignment issues resolved

--- lib/features/get_started_screen/first_screen.dart
+++ lib/features/get_started_screen/first_screen.dart
@@ -10,6 +10,8 @@ class FirstScreen extends StatelessWidget {
       backgroundColor: const Color(0xFFFDB515).withOpacity(0.82),
       body: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
               padding: const EdgeInsets.all(20.0),
@@ -28,29 +30,49 @@ class FirstScreen extends StatelessWidget {
                       Image.asset(
                         'assets/logo.png',
                         alignment: Alignment.centerRight,
-                        width: mediaQuery.size.width * 0.25,
+                        width: mediaQuery.size.width * 0.15,
                       ),
                       const SizedBox(
                         width: 20,
                       ),
-                      const Text(
-                        'KHUDA KA KALAM',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                      Text(
+                        'خدا کا کلام',
+                        style: TextStyle(
+                          color: const Color(0xFFFDB515).withOpacity(0.82),
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Jameel Noori Nastaleeq Kasheeda',
+                          fontSize: 50,
+                        ),
+                        textAlign: TextAlign.right,
+                        textDirection: TextDirection.rtl,
                       ),
                     ],
+                  ),
+                  const Text(
+                    'توریت زبور اور انجیل پر مبنی کورسز',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Jameel Noori Nastaleeq Kasheeda',
+                        fontSize: 30),
+                    textAlign: TextAlign.right,
+                    textDirection: TextDirection.rtl,
                   ),
                   const SizedBox(
                     height: 50,
                   ),
                   Container(
                     padding: const EdgeInsets.all(20.0),
-                    width: mediaQuery.size.height*0.5,
-                    height: mediaQuery.size.height*0.6,
+                    width: mediaQuery.size.height * 0.5,
+                    height: mediaQuery.size.height * 0.6,
                     alignment: Alignment.center,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(32),
-                      color:
-                          const Color(0xffffdcbc),
+                      color: const Color(0xffffdcbc),
+                    ),
+                    child: Image.asset(
+                      'assets/sun_logo.png',
+                      alignment: Alignment.centerRight,
+                      width: mediaQuery.size.width * 0.15,
                     ),
                   ),
                 ],
