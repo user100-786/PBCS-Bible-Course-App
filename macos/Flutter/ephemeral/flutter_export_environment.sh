ccebdc487590573bc59215a970295a33c3d155d3
Muhammad Saqlain <muhammadsaqlain.dev@gmail.com>
2023-06-11 11:47:00 +0500

    .

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=G:\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\DigiXtra Mobile Projects\Code\PBCS-Bible-Course-App"
+export "FLUTTER_ROOT=D:\Flutter Development\Tools\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Flutter Development\Projects\PBCS-Bible-Course-App"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
